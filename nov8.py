fe68cf44fd8767e7c0a9c8dd6d498a138eb7ee27
Constantine1126 <44215805+Constantine1126@users.noreply.github.com>
2018-11-08 09:40:56 +0800

    Add files via upload

---
+++ nov8.py
@@ -0,0 +1,104 @@
+def largest(list_of_numbers):
+    '''
+    Return the largest number in list_of_numbers
+
+    >>>largest([1,2,3,4])
+    4
+    '''
+    i=""
+    for i in range (0,list_of_numbers):
+        if i > list_of_numbers and i==i
+    return i
+
+
+def remove_duplicates(list1):
+    '''
+    Return a copy of old_list which has no dulicate element
+
+    >>>remove_duplicates([1,2,2,3])
+    [1,2,3]
+    '''
+    i=""
+    for item in list:
+        if i==item:
+            item.remove(list1)
+
+
+def common_elemnt (list1,list2):
+    '''
+    return true if and only if list1 and list2 have at least one element in common
+
+    >>>common_element([1,2,3],[1,4,5])
+    True
+    '''
+    for i in list1 and list2:
+       if list1[i]==list2[i]
+    return True
+    else
+    return False
+
+
+def list_to_string(list_of_numbers):
+    '''
+    Return a string consisting of each element of list_of_number
+
+    >>>list_to_string([1,2,3,4])
+    ['1','2','3','4']
+    '''
+    for item in list_of_numbers:
+        type(item)==str(item)
+    return item
+
+
+def extend_a_list(list1,list2):
+    '''
+    Add all of the element of list2 to the end of list1
+
+    >>>extend_a_list([1,2,3],[4,5,6])
+    [1,2,3,4,5,6]
+    '''
+    new_list[]
+    for i in list1 and list2:
+        new_list.append[i]
+
+
+def all_squares(max_number):
+    '''
+    Return a list of all the squares of numbers from 0 to max_number
+
+    >>>all_squares(26)
+    [0,1,2,3,4,5]
+    '''
+    new_list=[]
+    for j in range(0,max_number):
+        if j**2<=max_number:
+            new_list.append(j**2)
+
+
+def items_in_common(list1,list2):
+    '''
+    Return a list consisting of all the element that list1 and list2 have in common
+
+    >>>items_in_common([1,2,3],[1,4,5])
+    [1]
+    '''
+    new_list[]
+    for i in list1 and list2:
+        if list1[i]==list2[i]:
+    return new_list[i]
+
+
+def mystery_12(list_of_numbers,upper_limit)->list
+    '''
+    Return True if and only if the numbers in the list is lesser than the limit
+
+    >>>mystery_12([1,2,3],5)
+    True
+    '''
+    b=True
+    for e in list_of_numbers:
+        if e > upper_limit:
+            b=False
+    return b
+        
+    
